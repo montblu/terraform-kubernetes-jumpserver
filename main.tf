1f6fc08eeee87501ac1927b6154b3bff03f9d459
MiguelNdeCarvalho <geral@miguelndecarvalho.pt>
2023-10-26 13:50:24 +0100

    main.tf: Add config map with needed configs

--- main.tf
+++ main.tf
@@ -16,3 +16,16 @@ X11Forwarding no
 HostKey /config/ssh_host_keys/ssh_host_rsa_key
     EOT
 }
+
+resource "kubernetes_config_map" "main" {
+  metadata {
+    name      = local.resource_name
+    namespace = var.namespace
+  }
+
+  data = {
+    "motd"        = "Welcome to ${var.motd_name}"
+    "ssh_keys"    = var.ssh_keys
+    "sshd_config" = var.sshd_config == "" ? local.sshd_config : var.sshd_config
+  }
+}
