130a7da1fd5dde632fd351eff2db06b12884b70d
MiguelNdeCarvalho <geral@miguelndecarvalho.pt>
2023-10-26 11:18:17 +0100

    Add variables.tf

---
+++ variables.tf
@@ -0,0 +1,10 @@
+variable "name" {
+  type        = string
+  default     = "jumpserver"
+  description = "Name of the resource. Defaults to 'jumpserver'"
+}
+
+variable "name_prefix" {
+  type        = string
+  description = "Prefix of the resource."
+}
