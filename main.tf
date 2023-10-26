5f4ac8b512838ce257dde1f17f758c6f46732aeb
MiguelNdeCarvalho <geral@miguelndecarvalho.pt>
2023-10-26 13:43:01 +0100

    main.tf: Add sshd_config local

--- main.tf
+++ main.tf
@@ -1,3 +1,18 @@
 locals {
   resource_name = var.name_prefix == "" ? var.name : "${var.name_prefix}-${var.name}"
+
+  # Default SSH config
+  sshd_config = <<-EOT
+AllowTcpForwarding yes
+AuthorizedKeysFile      .ssh/authorized_keys
+ClientAliveCountMax 100
+ClientAliveInterval 30
+GatewayPorts clientspecified
+PasswordAuthentication no
+PermitTunnel yes
+PidFile /config/sshd.pid
+TCPKeepAlive no
+X11Forwarding no
+HostKey /config/ssh_host_keys/ssh_host_rsa_key
+    EOT
 }
